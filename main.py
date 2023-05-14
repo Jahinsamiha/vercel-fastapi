d859c198ec5985fb2e7e3b6fc02297cb5f2ce199
Jahinsamiha <jahin.samiha@stonybrook.edu>
2023-05-14 04:28:35 +0000

    added a fastapi file

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
 
+
 app = FastAPI()
 
+
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def get_icd_codes():
+    icd1 = {
+        "code": "A01.4",
+        "diagnosis": "Paratyphoid",
+        "billable":
+        "(effective 10/1/2015)"
+    }
+    icd2 = {
+        "code": "A02.9",
+        "diagnosis": "typhimurium",
+        "billable":
+        "(effective 10/1/2015)"
+    }
+    return {"icd1": icd1, "icd2": icd2}
+
